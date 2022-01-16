8eda3982577f87b32bc055dc06d2d5180c1f1f7f
Amr Hammam <amrh1095@gmail.com>
2022-01-16 17:32:37 -0500

    updated appicon and changed bundle indentifier

--- android/app/src/main/java/com/amrhammam/CultureMe/MainApplication.java
+++ android/app/src/main/java/com/amrhammam/CultureMe/MainApplication.java
@@ -1,4 +1,4 @@
-package com.amrdrensys.CultureMe;
+package com.amrhammam.CultureMe;
 
 import android.app.Application;
 import android.content.Context;
@@ -79,7 +79,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.amrdrensys.CultureMe.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.amrhammam.CultureMe.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
